23baac8fc4aacd62f790ddf42a47ebbaa5fdfe17
Amit Mishra <65525769+DaSeeker67@users.noreply.github.com>
2024-11-27 22:20:24 +0530

    loginpage

--- RolebasedSignp/src/Pages/LoginPage.jsx
+++ RolebasedSignp/src/Pages/LoginPage.jsx
@@ -11,7 +11,7 @@ const LoginPage = () => {
     const handleLogin = async (e) => {
         e.preventDefault();
         try {
-            const response = await axios.post("http://localhost:5000/api/auth/login", { email, password });
+            const response = await axios.post("https://rbac-czit.onrender.com/api/auth/login", { email, password });
             localStorage.setItem("token", response.data.token);
             navigate("/dashboard");
         } catch (err) {
